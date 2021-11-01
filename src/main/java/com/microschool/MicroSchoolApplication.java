fe4adbd93d50cf4cde9f360f453d79d0b5770756
yyangbobo <1360664142@qq.com>
2021-11-01 21:33:43 +0800

    start

---
+++ src/main/java/com/microschool/MicroSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.microschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroSchoolApplication.class, args);
+    }
+
+}

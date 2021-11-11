a24f9f0c40531a7b240bf87411dea8420952280b
yyangbobo <1360664142@qq.com>
2021-11-11 10:45:10 +0800

    mgb

--- school-mbg/src/main/java/com/school/generate/SchoolMbgApplication.java
+++ school-mbg/src/main/java/com/school/generate/SchoolMbgApplication.java
@@ -1,4 +1,4 @@
-package com.example.schoolmbg;
+package com.school.generate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ school-mbg/src/main/java/com/school/generate/mbg/CommentGenerator.java
@@ -0,0 +1,5 @@
+package com.school.generate.mbg;
+
+public class CommentGenerator {
+
+}

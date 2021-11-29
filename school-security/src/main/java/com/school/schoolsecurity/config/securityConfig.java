0196ca6c4ebca2c887431395238cba94afbbcc27
yyangbobo <1360664142@qq.com>
2021-11-29 22:02:00 +0800

    security

--- school-security/src/main/java/com/school/schoolsecurity/config/securityConfig.java
+++ school-security/src/main/java/com/school/schoolsecurity/config/securityConfig.java
@@ -2,20 +2,30 @@ package com.school.schoolsecurity.config;
 
 
 import com.school.schoolsecurity.Component.*;
+import com.school.schoolsecurity.util.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /*
 * created by yangbo
 * 2021/11/18
 * 对SpringSecurity的配置的扩展，支持自定义白名单资源路径和查询用户逻辑
+*
+*  该类是spring的核心方法   继承WebSecurityConfigurerAdapter  重写
+*  对SpringSecurity的配置的扩展，支持自定义白名单资源路径和查询用户逻辑
 * */
 public class securityConfig extends WebSecurityConfigurerAdapter {
 
@@ -23,16 +33,77 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
     private DynamicSecurityService dynamicSecurityService;
 
 
-    //
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
+        //指定了任何用户都可以访问的多个URL模式。
         ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry = httpSecurity
                 .authorizeRequests();
+
+        //不需要保护的资源路径允许访问
+        for (String url : ignoreUrlsConfig().getUrls()) {
+            registry.antMatchers(url).permitAll();
+        }
+
+        //允许跨域请求的OPTIONS请求
+        registry.antMatchers(HttpMethod.OPTIONS)
+                .permitAll();
+
+        // 任何请求需要身份认证
+        registry.and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                // 关闭跨站请求防护及不使用session
+                .and()
+                .csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                // 自定义权限拒绝处理类
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(restfulAccessDeniedHandler())
+                .authenticationEntryPoint(restAuthenticationEntryPoint())
+                // 自定义权限拦截器JWT过滤器
+                .and()
+                .addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+
+
         //有动态权限配置时添加动态权限校验过滤器
         if(dynamicSecurityService!=null){
             registry.and().addFilterBefore(dynamicSecurityFilter(), FilterSecurityInterceptor.class);
         }
     }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter() {
+        return new JwtAuthenticationTokenFilter();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public RestfulAccessDeniedHandler restfulAccessDeniedHandler() {
+        return new RestfulAccessDeniedHandler();
+    }
+
+    @Bean
+    public RestAuthenticationEntryPoint restAuthenticationEntryPoint() {
+        return new RestAuthenticationEntryPoint();
+    }
+
+
 
     @ConditionalOnBean(name = "dynamicSecurityService")
     @Bean
@@ -40,11 +111,13 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
         return new DynamicAccessDecisionManager();
     }
 
+
     @ConditionalOnBean(name = "dynamicSecurityService")
     @Bean
     public DynamicSecurityFilter dynamicSecurityFilter() {
         return new DynamicSecurityFilter();
     }
+
 
     @ConditionalOnBean(name = "dynamicSecurityService")
     @Bean
@@ -52,6 +125,16 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
         return new DynamicSecurityMetadataSource();
     }
 
+
+    @Bean
+    public IgnoreUrlsConfig ignoreUrlsConfig() {
+        return new IgnoreUrlsConfig();
+    }
+
+    @Bean
+    public JwtTokenUtil jwtTokenUtil() {
+        return new JwtTokenUtil();
+    }
 
 
 
